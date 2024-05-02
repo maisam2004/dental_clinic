d9c1075472644515dca08a1c856f16fe3eee4c54
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-05-02 11:00:38 +0100

    Modify datepicker modify date and time widgets

--- dentalhealthyclinic/appointments/forms.py
+++ dentalhealthyclinic/appointments/forms.py
@@ -1,21 +1,26 @@
 from django import forms
 from .models import Service,Appointment
 from bootstrap_datepicker_plus.widgets import DateTimePickerInput,DatePickerInput,TimePickerInput
-from datetime import date
+from datetime import date,datetime
+from datetime import date, timedelta  
 
 class AppointmentForm(forms.ModelForm):
     class Meta:
         model = Appointment
         fields = ['full_name', 'phone_number', 'email',  'dentist', 'date', 'time', 'service', 'notes']  # Adjust fields as needed
         widgets = {
-            #'start_date': DateTimePickerInput(options={"format": "DD-MM-YYYY"}),
+            
             'date': DatePickerInput(options={
-                    "format": "dd-mm-yyyy",  # Format for day, month, and year
-  
-                }),
+                    "format": "DD-MM-yyy",  # Format for day, month, and year
+                    "minDate": date.today() + timedelta(days=1),  # Key change
+                    "daysOfWeekDisabled": [0, 6], 
+                    }),
+
             'time': TimePickerInput(options={
-                    "format": "mm-hh",  # Format for day, month, and year
-  
+                    "format": "HH:mm",  # Format for day, month, and year
+                    "stepping": 15,
+                    "enabledHours": [ 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+                    
                 }),
         }
 
