529b8971b3c8905f8704b81875071a49b2f15db7
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-05-02 17:29:07 +0100

    Add navbar and hero part image

--- dentalhealthyclinic/dentalhealthyclinic/urls.py
+++ dentalhealthyclinic/dentalhealthyclinic/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +24,5 @@ urlpatterns = [
     path('', include('core.urls')),
     path('appointments/', include('appointments.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
