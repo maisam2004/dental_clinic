56749e159d816b0fe07cc19678d83ebf2e42df08
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-05-05 11:10:33 +0100

    Added product details function and page

--- dentalhealthyclinic/products/views.py
+++ dentalhealthyclinic/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Product
 # Create your views here.
 def all_products(request):
@@ -9,3 +9,10 @@ def all_products(request):
     }
     return render(request,'products/products.html',context)
 
+def product_detail(request,product_id):
+    '''This show  product details of indivisual product '''
+    product = get_object_or_404(Product,pk=product_id)
+    context = {
+        'product':product
+    }
+    return render(request,'products/product_detail.html',context)
