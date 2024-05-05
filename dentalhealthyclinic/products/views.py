5d3e22dcac6951f87bb840a5ea8c3c33eb82ad28
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-05-05 22:02:12 +0100

    Added back to up button

--- dentalhealthyclinic/products/views.py
+++ dentalhealthyclinic/products/views.py
@@ -2,20 +2,22 @@ from django.shortcuts import render,get_object_or_404,redirect,reverse
 from django.contrib import messages
 from .models import Product,Category
 from django.db.models import Q
+from django.db.models.functions import Lower 
 # Create your views here.
 def all_products(request):
-    '''This view show all product of database'''
+    '''This view show all product of database,add order by  sort by and searching products  '''
     products = Product.objects.all()
     query=None
     category = None
     categories=None
+    #sort_by = request.GET.get('sort_by', 'default')
     if request.GET:
-        if 'category' in request.GET:
+        if 'category' in request.GET: #check if there are category word in request
             rcategories = request.GET['category'].split(',')
-            products = products.filter(category__name__in = rcategories)
-            categories = Category.objects.filter(name__in = rcategories)
+            products = products.filter(category__name__in = rcategories)#filter by using categroy model name field
+            categories = Category.objects.filter(name__in = rcategories)#find the categories name to show in context as current_category
 
-        if 'searchbar' in request.GET:
+        if 'searchbar' in request.GET:  #check if there are searchbar word in request
             query = request.GET['searchbar']
             if not query:
                 messages.error(request,'You did not enter valid serch')
@@ -24,8 +26,35 @@ def all_products(request):
                 queries = Q(name__icontains=query) | Q(description__icontains=query)
                 products=products.filter(queries)
 
+        """
+        if sort_by == 'name_asc':
+            products = products.order_by(Lower('name'))
+        elif sort_by == 'name_desc':
+            products = products.order_by(Lower('name').desc())
+        elif sort_by == 'price_asc':
+            products = products.order_by('price')
+        elif sort_by == 'price_desc':
+            products = products.order_by('-price')
+        elif sort_by =='rating':
+            products = products.order_by('-rating')
+        elif sort_by =='category':
+            products = products.order_by('category') 
+        
+        """ 
+        sort_mapping = {
+                'default': 'pk',  # Default could be by primary key or any other field
+                'name_asc': Lower('name'),
+                'name_desc': Lower('name').desc(),
+                'price_asc': 'price',
+                'price_desc': '-price',
+                'rating': '-rating',
+                'category': 'category',
+            }
+        
+        sort_by = request.GET.get('sort_by', 'default')
+        products = products.order_by(sort_mapping.get(sort_by))  
     context = {
-        'products':products,'search_term':query,'currnet_category':categories
+        'products':products,'search_term':query,'currnet_category':categories,'sort_by': sort_by
     }
     return render(request,'products/products.html',context)
 
