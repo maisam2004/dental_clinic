4d64866e304d85ed84e81f9932d0eef8f021a453
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-05-07 11:00:58 +0100

    Added add to bag functionality

--- dentalhealthyclinic/basket/views.py
+++ dentalhealthyclinic/basket/views.py
@@ -5,3 +5,18 @@ from django.shortcuts import render,redirect
 def view_basket(request):
     """ A view that renders the basket contents """
     return render(request,'basket/basket.html')
+
+def add_to_basket(request, item_id):
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+
+    print(request.session['bag'])
+    return redirect(redirect_url)
