8c08da39a8c9facb1d4dc4cbe6c822e8d30859dd
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-05-11 15:23:42 +0100

    Add decouple package

--- dentalhealthyclinic/dentalhealthyclinic/settings.py
+++ dentalhealthyclinic/dentalhealthyclinic/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
