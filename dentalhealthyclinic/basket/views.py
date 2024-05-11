6014746521c54b3116a7f63ccb93948af965fbed
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-05-11 11:14:30 +0100

    Added Noticification css to shopping bag

--- dentalhealthyclinic/basket/views.py
+++ dentalhealthyclinic/basket/views.py
@@ -24,20 +24,21 @@ def view_basket(request):
         'bag_items': bag_items,
         # ... other variables for your template (total, delivery, etc.)
     }
-    print(bag_items)
-    print(request.session['bag'])
+    #print(bag_items)
+    #print(request.session['bag'])
     return render(request, 'basket/basket.html', context) 
 
 
 def add_to_basket(request, item_id):
     product = Product.objects.get(pk=item_id)
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
-        messages.success(request,f'Update quantity of  {product.name} in you basket')
+        messages.success(request,f'Update quantity of  {product.name} ,now you have  {bag[item_id]} of this item in basket')
     else:
         bag[item_id] = quantity
         messages.success(request,f'Added {product.name} to you bag')
@@ -49,19 +50,24 @@ def add_to_basket(request, item_id):
 
 
 def update_basket(request):
+    
     if request.method == 'POST':
         item_id = request.POST.get('item_id')
         action = request.POST.get('action')
         bag = request.session.get('bag', {})
-
+        
+        product = Product.objects.get(pk=item_id)
+        
         if action == 'remove':
             if item_id in bag:
                 del bag[item_id]
+                messages.warning(request,f'removed {product.name} from you basket ')
         elif action == 'update':
             quantity = int(request.POST.get('quantity'))
             if item_id in bag:
                 bag[item_id] = quantity
-
+                
+                
         request.session['bag'] = bag
 
     return redirect('view_basket')
