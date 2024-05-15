f66fe1b91359dfae4c2a0224a2e5a517d73095a2
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-05-15 12:00:16 +0100

    Added overlay loading and style

--- dentalhealthyclinic/pay/static/pay/js/stripe_elements.js
+++ dentalhealthyclinic/pay/static/pay/js/stripe_elements.js
@@ -1,10 +1,19 @@
-const stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-const client_secret = $('#id_client_secret').text().slice(1,-1);
-const stripe = Stripe(stripe_public_key);
-const elements =stripe.elements();
+const stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+const clientSecret = $('#id_client_secret').text().slice(1,-1);
+const stripe = Stripe(stripePublicKey);
+const elements = stripe.elements();
 
 
-let style = {
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
+
+const style = {
     base: {
         color: '#000',
         fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
@@ -19,12 +28,9 @@ let style = {
         iconColor: '#dc3545'
     }
 };
+const card = elements.create('card', {style: style});
+card.mount('#card-element');
 
-const card = elements.create('card',{style:style})
-
-
-card.mount('#card-element');  
- 
 // Handle realtime validation errors on the card element
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
@@ -39,4 +45,38 @@ card.addEventListener('change', function (event) {
     } else {
         errorDiv.textContent = '';
     }
+});
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
 });
