81d4ad2c5b2cb1d08fba4eb6454f78d510d3fadc
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-05-27 20:34:21 +0100

    Add data to fee model and update services

--- dentalhealthyclinic/fee/models.py
+++ dentalhealthyclinic/fee/models.py
@@ -12,10 +12,10 @@ class Category(models.Model):
         return self.name
 
 class Fee(models.Model):
-    service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='fees')
+    service = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='fees')
     description = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return f"{self.service.name} - {self.description}"
+        return f"{self.service} - {self.description}"
