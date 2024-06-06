5483fd6bef413061d0506686cc4f4aa01b60fc4e
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-06-07 00:37:11 +0100

    Modify forms for chaeckout placce holder error

--- dentalhealthyclinic/pay/forms.py
+++ dentalhealthyclinic/pay/forms.py
@@ -20,7 +20,7 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': '',
+            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
@@ -31,6 +31,14 @@ class OrderForm(forms.ModelForm):
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if field != 'country':
+                placeholder = f'{placeholders[field]} *' if self.fields[field].required else placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
+
+
+"""    for field in self.fields:
+            if field != 'country':
                 if self.fields[field].required:
                     placeholder = f'{placeholders[field]} *'
                 else:
@@ -39,4 +47,4 @@ class OrderForm(forms.ModelForm):
                 placeholder = f'{placeholders[field]}'
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-            self.fields[field].label = False
+            self.fields[field].label = False """
