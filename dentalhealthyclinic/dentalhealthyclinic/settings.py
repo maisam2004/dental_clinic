e9d2f87ceb9c37c0403ead42d7cd672e9a6d6b4e
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-06-10 14:06:44 +0100

    Update staticfiles folder added all static

--- dentalhealthyclinic/dentalhealthyclinic/settings.py
+++ dentalhealthyclinic/dentalhealthyclinic/settings.py
@@ -108,6 +108,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- dentalhealthyclinic/dentalhealthyclinic/wsgi.py
+++ dentalhealthyclinic/dentalhealthyclinic/wsgi.py
@@ -17,3 +17,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dentalhealthyclinic.settings')
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
+
