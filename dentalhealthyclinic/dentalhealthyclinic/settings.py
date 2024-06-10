78a1ff485ebefc023b63a1cab9aacd912d4b3c9e
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-06-10 13:46:04 +0100

    Update and install whitenoise app for staticfiles

--- dentalhealthyclinic/dentalhealthyclinic/settings.py
+++ dentalhealthyclinic/dentalhealthyclinic/settings.py
@@ -119,6 +119,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "allauth.account.middleware.AccountMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 
     
 ]
--- dentalhealthyclinic/dentalhealthyclinic/wsgi.py
+++ dentalhealthyclinic/dentalhealthyclinic/wsgi.py
@@ -6,11 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
+from whitenoise import WhiteNoise
 
 import os
-
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dentalhealthyclinic.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
