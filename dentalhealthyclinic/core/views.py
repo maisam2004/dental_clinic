cb40be271a6e1a88b16b3aeb58cd629db8478a8f
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-06-12 13:21:22 +0100

    Added animation to partners and companies

--- dentalhealthyclinic/core/views.py
+++ dentalhealthyclinic/core/views.py
@@ -5,6 +5,6 @@ def homepage(request):
     """ Renders the homepage of the website.
 
     This view function handles the rendering of the homepage template. """
-    context = {} 
+    context = {'is_homepage': True,} 
 
     return render(request, 'core/homepage.html', context)
