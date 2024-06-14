35a58b9ed3adfd4fa3bd7da04fb572020735c610
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-06-14 18:47:00 +0100

    Update bucket name for aws

--- dentalhealthyclinic/dentalhealthyclinic/settings.py
+++ dentalhealthyclinic/dentalhealthyclinic/settings.py
@@ -39,7 +39,7 @@ ALLOWED_HOSTS = ['healtydentalclinic-8f35f633fd68.herokuapp.com','localhost','12
 #digital ocean 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+
 AWS_S3_ENDPOINT_URL = config('AWS_S3_ENDPOINT_URL')  #  endpoint 
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
