fd033fde5117fc7f9e2982ba272bd0920f4d76c2
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-06-14 19:43:57 +0100

    Update media storages modifications

--- dentalhealthyclinic/custom_storages.py
+++ dentalhealthyclinic/custom_storages.py
@@ -6,9 +6,6 @@ class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
 
 
-#class MediaStorage(S3Boto3Storage):
-   # location = settings.MEDIAFILES_LOCATION
-
 class MediaStorage(S3Boto3Storage):
-    location = 'media'
-    file_overwrite = False
+    location = settings.MEDIAFILES_LOCATION
+
