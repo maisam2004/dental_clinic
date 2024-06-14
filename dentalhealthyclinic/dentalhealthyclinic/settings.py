bb3e66db852d17e8cdcbb2f2ae2be494a5b39f17
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-06-15 00:16:28 +0100

    Update media caching control

--- dentalhealthyclinic/dentalhealthyclinic/settings.py
+++ dentalhealthyclinic/dentalhealthyclinic/settings.py
@@ -260,6 +260,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if 'USE_AWS' in os.environ:
 
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT', #do not clear cach
+        'CacheControl': 'max-age=94608000',
+    }
+
+
     AWS_STORAGE_BUCKET_NAME ="healtydentalclinic"
     AWS_S3_REGION_NAME = "eu-north-1"
     AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
