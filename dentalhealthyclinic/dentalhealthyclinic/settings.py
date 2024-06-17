e48faed72f7b77ee096168a0c73aa8860ce84d1e
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-06-17 21:18:46 +0100

    Set debug to True

--- dentalhealthyclinic/dentalhealthyclinic/settings.py
+++ dentalhealthyclinic/dentalhealthyclinic/settings.py
@@ -32,8 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY=config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
-
+DEBUG = True
 ALLOWED_HOSTS = ['healtydentalclinic-8f35f633fd68.herokuapp.com','localhost','127.0.0.1']
 
 
